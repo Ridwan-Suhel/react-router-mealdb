9d8108b184126a0d3126aea06410329d406a273d
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-01 01:04:38 +0600

    useNavigate added

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Meal.css";
 const Meal = (props) => {
   const { strMeal, strInstructions, strMealThumb, idMeal } = props.meal;
+  const navigate = useNavigate();
+  const foodDetails = () => {
+    const path = `/meal/${idMeal}`;
+    navigate(path);
+  };
 
   return (
     <div className="single-box">
       <img src={strMealThumb} alt="img of meal" />
       <h3>Meal Name: {strMeal}</h3>
       <p>Instruction: {strInstructions.slice(0, 100)}</p>
-      {/* <button className="detail-btn" onClick={foodDetails}>Detail Food</button> */}
+      <button className="detail-btn" onClick={foodDetails}>
+        Food Detail - {idMeal}
+      </button>
       <Link className="details-link" to={`/meal/${idMeal}`}>
         Food Details
       </Link>
